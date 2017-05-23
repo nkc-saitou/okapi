f5a5e5bf7a5d08c35b7b4fc1a79df226e7fa2683
DESKTOP-A792KI4\user <nkc.yuki.saitou@gmail.com>
2017-05-23 09:42:58 +0900

    キャラクターの移動

--- PoyoPoyon/Assets/saiyuuki/Script/PlayerMove.cs
+++ PoyoPoyon/Assets/saiyuuki/Script/PlayerMove.cs
@@ -4,28 +4,42 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    //-------------------------------------
+    // 定数
+    //-------------------------------------
+
+    const float FLICK_DIRECTION = 20; //フリックする距離
 
     //-------------------------------------
     // public
     //-------------------------------------
-    public GameObject[] soilder;
+
+    public GameObject[] soldier;
+
     //-------------------------------------
     // private
     //-------------------------------------
+
     Ray ray;
     RaycastHit2D hit;
 
+    GameObject[] touchObj = new GameObject[2]; //0がRight,1がLeft
     GameObject clickObj;
 
-    Vector3[] soilderStartPos = new Vector3[2]; //初期位置
+    Vector3[] soldierStartPos = new Vector3[2]; //初期位置
+    Vector3 touchStartPos;
+    Vector3 touchEndPos;
 
-    ClickState state = 0;
+    bool[] touchFlg = new bool[2];
+
+    ClickState clickState = 0;
 
     void Start()
     {
         for(int i =0;i<2;i++)
         {
-            soilderStartPos[i] = soilder[i].transform.position;
+            soldierStartPos[i] = soldier[i].transform.position;
+            touchFlg[i] = false;
         }
     }
 
@@ -35,27 +49,29 @@ public class PlayerMove : MonoBehaviour
         {
             foreach (Touch t in Input.touches)
             {
-                int id = t.fingerId;
 
                 switch (t.phase)
                 {
                     //タッチしたとき
                     case TouchPhase.Began:
+                        Touch(t.fingerId);
                         break;
 
                     //タッチされているとき
                     case TouchPhase.Moved:
+                        TouchDrag(t.fingerId);
                         break;
 
                     //タッチされていないとき
                     case TouchPhase.Ended:
+                        TouchEnd(t.fingerId);
                         break;
                 }
             }
         }
         else
         {
-            switch (state)
+            switch (clickState)
             {
                 case ClickState.Click:
                     MouseClick();
@@ -67,6 +83,74 @@ public class PlayerMove : MonoBehaviour
             }
         }
     }
+    //--------------------------------------------
+    // Touch用メソッド
+    //--------------------------------------------
+    void Touch(int id)
+    {
+        //rayを生成
+        ray = Camera.main.ScreenPointToRay(Input.touches[id].position);
+        //rayの衝突判定
+        hit = Physics2D.Raycast(ray.origin, ray.direction);
+
+        for (int i = 0; i < 2; i++)
+        {
+            if (hit.collider)
+            {
+                ////rayがあたったオブジェクトを保存
+                //touchObj[id] = hit.collider.gameObject;
+                if (hit.collider.gameObject == soldier[i])
+                {
+                    touchFlg[i] = true;
+                }
+
+                //else if(hit.collider.gameObject == soldier[1])
+                //{
+                //    touchFlg[1] = true;
+                //}
+            }
+        }
+    }
+
+    void TouchDrag(int id)
+    {
+        //タッチした位置を保存
+        Vector3 TouchPos = Input.touches[id].position;
+        TouchPos.z = 10;
+        //タッチした位置をワールド座標へ変換
+        Vector3 soldierObjPos = Camera.main.ScreenToWorldPoint(TouchPos);
+
+        if (touchFlg[id])
+        {
+            soldierObjPos.x = soldierStartPos[id].x;
+            soldier[id].transform.position = soldierObjPos;
+        }
+
+        if (hit.collider == null && touchFlg[id])
+        {
+            touchFlg[id] = false;
+        }
+        
+
+        //if(touchFlg[1])
+        //{
+        //    soldierObjPos.x = soldierStartPos[1].x;
+        //    soldier[1].transform.position = soldierObjPos;
+        //}
+    }
+
+    void TouchEnd(int id)
+    {
+
+        //if (hit.collider == null && touchFlg[id])
+        //{
+        //    touchFlg[id] = false;
+        //}
+        
+    }
+    //--------------------------------------------
+    // Click用メソッド
+    //--------------------------------------------
 
     void MouseClick()
     {
@@ -80,7 +164,7 @@ public class PlayerMove : MonoBehaviour
             //rayが当たったオブジェクトを保存
             clickObj = hit.collider.gameObject;
             //状態をドラッグに
-            state = ClickState.Drag;
+            clickState = ClickState.Drag;
         }
     }
 
@@ -93,21 +177,60 @@ public class PlayerMove : MonoBehaviour
                 Vector3 clickPos = Input.mousePosition;
                 clickPos.z = 10;
 
-                if (clickObj == soilder[soilderNo])
+                if (clickObj == soldier[soilderNo])
                 {
                     //クリックした位置へ移動させる
                     Vector3 soilderPos = Camera.main.ScreenToWorldPoint(clickPos);
-                    soilderPos.x = soilderStartPos[soilderNo].x;
-                    soilder[soilderNo].transform.localPosition = soilderPos;
+                    soilderPos.x = soldierStartPos[soilderNo].x;
+                    soldier[soilderNo].transform.localPosition = soilderPos;
+                    Flick();
                 }
             }
         }
         else if (Input.GetMouseButtonUp(0))
         {
             //状態をクリック待ちにする
-            state = ClickState.Click;
+            clickState = ClickState.Click;
         }
-        
+    }
+
+    void Flick()
+    {
+        //マウスをクリックしたポジションの保存
+        if(Input.GetMouseButtonDown(0))
+        {
+            touchStartPos = new Vector3(
+                Input.mousePosition.x,
+                Input.mousePosition.y,
+                Input.mousePosition.z);
+        }
+        //マウスから指を離したポジションの保存
+        else if(Input.GetMouseButtonUp(0))
+        {
+            touchEndPos = new Vector3(
+                Input.mousePosition.x,
+                Input.mousePosition.y,
+                Input.mousePosition.z);
+        }
+
+        GetDirection();
+    }
+
+    void GetDirection()
+    {
+        float dir = touchEndPos.x - touchStartPos.x;
+        //右フリック
+        if(FLICK_DIRECTION < dir)
+        {
+            //LeftObjがRightObjの方にいく処理
+            Debug.Log("Right");
+        }
+        //左フリック
+        else if(-FLICK_DIRECTION > dir)
+        {
+            //RightObjがLeftObjの方に行く処理
+            Debug.Log("Left");
+        }
     }
 
 
