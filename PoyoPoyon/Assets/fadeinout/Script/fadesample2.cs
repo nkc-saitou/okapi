aaf18229474e3600c4aaed1c27a220626161c96c
keitatakahasi <nkc.keita.takahashi@gmail.com>
2017-05-25 14:26:39 +0900

    fade in out

---
+++ PoyoPoyon/Assets/fadeinout/Script/fadesample2.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fadesample2 : MonoBehaviour
+{
+    private float fadetime;
+	public void FadeScene (float fadetime)
+	{
+        //"sample"に移行したいシーン名を入力
+		FadeManager.Instance.LoadScene ("sample2", fadetime);
+        //移行するシーンがsampleのため"sample"
+	}
+}
