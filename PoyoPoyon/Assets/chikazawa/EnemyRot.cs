1ff32fdd8d7e2121353b2baaf43ebf82d2b925fa
DESKTOP-A792KI4\user <nkc.yuki.saitou@gmail.com>
2017-06-01 14:18:24 +0900

    α版

---
+++ PoyoPoyon/Assets/chikazawa/EnemyRot.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRot : MonoBehaviour
+{
+
+    public Transform playerL;    //プレイヤーを代入
+    public Transform playerR;
+    public bool swichtarget = false;
+    public Transform target; // 追いかける対象
+
+    Vector3 eWpos;
+
+    // Use this for initialization
+    void Start()
+    {
+        playerL = GameObject.FindGameObjectWithTag("lPlayer").transform;
+        playerR = GameObject.FindGameObjectWithTag("rPlayer").transform;
+    }
+
+
+    void Update()
+    {
+        transform.position = eWpos;
+        Vector3 p2 = target.position;
+        Vector3 p1 = transform.position;
+        if (swichtarget == false)
+        {
+            target = playerL;
+            eWpos = new Vector3(-0.5f, 1.0f, 0);
+        }
+        else
+        {
+            target = playerR;
+            eWpos = new Vector3(0.5f, 1.0f, 0);
+        }
+        float dx = p2.x - p1.x;
+        float dy = p2.y - p1.y;
+        float rad = Mathf.Atan2(dy, dx);
+
+        Vector3 ArrowAngle = new Vector3(0, 0, rad * Mathf.Rad2Deg);
+        float z = Mathf.LerpAngle(transform.eulerAngles.z, ArrowAngle.z, 0.1f);
+        transform.eulerAngles = new Vector3(0, 0, z);
+    }
+}
---
+++ PoyoPoyon/Assets/takahashi/Script/fadesample.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fadesample : MonoBehaviour
+{
+    private float fadetime;
+	public void FadeScene (float fadetime)
+	{
+        //"sample"に移行したいシーン名を入力
+		FadeManager.Instance.LoadScene ("sample", fadetime);
+        //移行するシーンがsampleのため"sample"
+	}
+}
