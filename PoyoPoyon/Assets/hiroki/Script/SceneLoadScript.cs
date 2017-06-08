2b05820b596b5fabbe6d7a24f2fda2205a3bb4d3
HirokiSito <nkc.hiroki.saito@gmail.com>
2017-06-08 14:12:47 +0900

    Title　sceneの実装

---
+++ PoyoPoyon/Assets/hiroki/Script/SceneLoadScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SceneLoadScript : MonoBehaviour
+{
+
+    public int SceneSpeed = 4;
+    void Start()
+    {
+        Move.soldierStartFlg = false;
+    }
+    void Update()
+    {
+        //画面のどこかをクリックしたときにゲーム開始
+        if (Input.GetMouseButtonDown(0))
+        {
+            Move.soldierStartFlg = true;
+            Invoke("title", SceneSpeed);
+        }
+    }
+    void title()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("main");
+    }
+}
