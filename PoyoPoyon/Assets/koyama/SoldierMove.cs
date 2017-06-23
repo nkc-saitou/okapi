008e8ed2292d486fc2b5eb7fef669763d0805d03
DESKTOP-A792KI4\user <nkc.yuki.saitou@gmail.com>
2017-06-23 13:59:17 +0900

    クレジット動かない

---
+++ PoyoPoyon/Assets/koyama/SoldierMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoldierMove : MonoBehaviour {
+
+    const float SOLDIER_WARP = 20.0f;
+
+    Vector2 soldierPos;
+    public float speed = 1.0f;
+
+	void Start ()
+    {
+
+	}
+
+    void Update()
+    {
+
+        soldierPos = transform.position;
+        soldierPos.y += speed;
+
+        if (soldierPos.y > SOLDIER_WARP)
+        {
+            soldierPos.y = -SOLDIER_WARP;
+        }
+        transform.position = soldierPos;
+
+
+    }
+}
